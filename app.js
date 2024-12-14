f0786052cdf1061960898afea012149bbd0b9d9b
Dmitry Kuchukov <kudim@inbox.ru>
2024-12-14 20:59:24 +0300

    refactor(basic-3.4): change into js file, delete tag selector

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="task-checkbox";
     editInput.type="text";
     editInput.className="task-input";
 
